22ca58783236bcc99165576793ccd0ad888c1f24
Mike Weaver <mike@weaverfamily.net>
2016-01-11 12:40:06 -0800

    Code review comments

--- main.rb
+++ main.rb
@@ -1,9 +1,14 @@
+# GENERAL COMMMENTS
+# The code would be more readable and maintable if it was broken up into functions
+
+# Variable name is misleading, this is a timestamp regex, not caption
 caption_reg = /\[\d+:\d+:\d+.\d+\]/
 caption_drop_reg = /^\s*\{BLANK\_AUDIO\}\s*$/
 
 ARGV.each do |path|
   puts "Analyizing files in Directory: #{path}"
   counter = 0
+  # The files will have different GUIDs after mp4: *.mp4_*.qt
   Dir.glob(path + '*.mp4_5823fb160c8346bc82ec90cc4d4472b1.qt') do |qt_file|
     counter += 1
     content = Array.new
@@ -29,12 +34,15 @@ ARGV.each do |path|
           end
         elsif in_caption==true
 
+          # The spec asked for parenthesis () not curly braces
           caption_block.push(line.gsub(/[\[\]]/, '[' => '{', ']' => '}'))
         else 
           content.push(line)
         end
       end
     end
+    #it would be safer to not overwrite the input files.
+    #What if the script makes a mistakes and you destroy your originals?
     #write contents over file
     File.open(qt_file, "w") do |f|
       f.puts content
@@ -44,6 +52,8 @@ ARGV.each do |path|
     filename = File.basename(qt_file, '.mp4_5823fb160c8346bc82ec90cc4d4472b1.qt')[4..-1]
     File.rename(qt_file, "#{path+filename}.qt.text")
 
+    # You didn't want to use string substitution?
+    # Reading the text from the file would make the script more flexable. (We could change the SMIL file as needed)
     #Create a smil
     smil = File.new("#{path+filename}.smil", "w")
     smil.puts '<?xml version="1.0" encoding="UTF-8"?>'
